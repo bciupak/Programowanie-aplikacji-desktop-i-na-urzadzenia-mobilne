f29fad219076f037e339b5b489ff185d940ae5ef
bciupak <01187523@pw.edu.pl>
2025-10-14 12:00:20 +0200

    commit 1

--- lab1/lab1/obj/Debug/net9.0/lab1.AssemblyInfo.cs
+++ lab1/lab1/obj/Debug/net9.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbeb87170fba54ad1fa7c7fa57dea9d72df04e66")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
